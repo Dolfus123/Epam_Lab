dbbb0e2e166f9f30f75e80a3ba3ae28c142af10b
Dolfus123 <84563959+Dolfus123@users.noreply.github.com>
2022-01-18 08:53:20 +0200

    Add files via upload

---
+++ Lab_9/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+
+
+namespace Lab_9
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       public void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                //openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                if (openFileDialog.ShowDialog() == true)
+                    TextBox.Text = File.ReadAllText(openFileDialog.FileName, Encoding.UTF8);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось открыть файл");
+                //throw;
+            }
+       }
+        public void btnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs";
+                saveFileDialog.OverwritePrompt = true;
+                if (saveFileDialog.ShowDialog() == true)
+                    File.WriteAllText(saveFileDialog.FileName, TextBox.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось сохранить файл");
+                //throw;
+            }
+        }
+    }
+}
